4c43861f772467f2e0b2fd05513949e8fd5e9999
momoph46 <92513778+momoph46@users.noreply.github.com>
2022-01-27 09:02:36 +0800

    Update mac.v

--- src/mac.v
+++ src/mac.v
@@ -1,11 +1,13 @@
 module mac (instruction, multiplier, multiplicand, stall, clk, reset_n, result, protect);
-input signed [15:0] multiplier;
-input signed [15:0] multiplicand; 
+input [15:0] multiplier;
+input [15:0] multiplicand; 
 input  clk;
 input  reset_n;
 input  stall;
 input  [2:0] instruction;
-output reg [31:0] result;
-output reg [7:0] protect;
+output [31:0] result;
+output [7:0] protect;
 
 //Add you design here
+
+endmodule
